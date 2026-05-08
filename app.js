0056467676de770131b6a67fc04ccd6ff1e97982
Amaedeyus <deleonfuego@gmail.com>
2026-05-08 13:46:35 +0800

    finals try

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const productRouter = require('./routes/productRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -13,7 +15,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes 
+// Routes
 app.use('/api/v1/products', productRouter); // Changed from '/' to '/api/v1/products'
+app.all('*', (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 
-module.exports = app; 
+module.exports = app;
